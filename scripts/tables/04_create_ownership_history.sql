810c3b66b31df26e589024e071da780c04984741
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-19 13:02:02 +0000

    feat: add SQL script to create Ownership History table
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ scripts/tables/04_create_ownership_history.sql
@@ -0,0 +1,11 @@
+-- Drop table if it exists
+DROP TABLE IF EXISTS OwnershipHistory;
+
+-- Create table
+CREATE TABLE OwnershipHistory (
+  OwnershipID INTEGER PRIMARY KEY AUTOINCREMENT,
+  OwnerID INTEGER NOT NULL, -- Mandatory foreign key
+  CarID INTEGER NOT NULL, -- Mandatory foreign key
+  StartDate TEXT(10) NOT NULL,
+  EndDate TEXT(10) NOT NULL
+);
