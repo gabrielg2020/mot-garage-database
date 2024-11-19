7176144a78491beb40c9e643c9a2630025212b8f
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-19 15:01:42 +0000

    fix: fix Car table
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- scripts/tables/03_create_car.sql
+++ scripts/tables/03_create_car.sql
@@ -7,8 +7,8 @@ CREATE TABLE Car (
   RegistrationNumber TEXT(7) NOT NULL,
   Make TEXT(20) NOT NULL,
   Model TEXT(50) NOT NULL,
-  Year TEXT(4) NOT NULL,
-  VIN INTEGER NOT NULL,
+  Year INTEGER(4) NOT NULL,
+  VIN INTEGER(17) NOT NULL,
   OwnerID INTEGER NOT NULL, -- Mandatory foreign key
   FOREIGN KEY (OwnerID) REFERENCES Owner(OwnerID)
 );
