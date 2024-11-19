e6569c35a71ef2dd4f3301081e161d5e4df65cbe
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-19 15:47:14 +0000

    feat: add SQL script to create advisory data
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ scripts/data/10_create_advisory_data.sql
@@ -0,0 +1,11 @@
+BEGIN TRANSACTION;
+
+-- Insert data
+INSERT INTO Advisory (Description, Severity) VALUES 
+  ('Oil change recommended', 'Low'),
+  ('Brake pads need replacement', 'High'),
+  ('Engine coolant level low', 'Medium'),
+  ('Tire pressure low', 'Low'),
+  ('Transmission fluid leak detected', 'High');
+
+COMMIT;
