9c1b860e4decc165220fae884048d40688780f7b
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-19 01:36:43 +0000

    feat: add SQL script to create Address table
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- scripts/tables/create_tables.sh
+++ scripts/tables/create_tables.sh
@@ -1,4 +1,7 @@
 # Run Database and run to build SQL tables
 
+
+# Address table
+sqlite3 database/mot_garage_db.sqlite < scripts/tables/create_address.sql
 # Owner table
 sqlite3 database/mot_garage_db.sqlite < scripts/tables/create_owner.sql
