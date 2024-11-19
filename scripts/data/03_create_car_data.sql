b56a93689befea603887b4ece5d18ff893736f69
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-19 22:53:25 +0000

    fix: remove owner dependency in Car table

--- scripts/data/03_create_car_data.sql
+++ scripts/data/03_create_car_data.sql
@@ -1,11 +1,11 @@
 BEGIN TRANSACTION;
 
 -- Insert data
-INSERT INTO Car (RegistrationNumber, Make, Model, Year, VIN, OwnerID) VALUES
-  ('AB12CDE', 'Ford', 'Focus', 2015, 12345678901234567, 1),
-  ('FG34HIJ', 'Vauxhall', 'Corsa', 2018, 23456789012345678, 2),
-  ('KL56MNO', 'BMW', '320i', 2017, 34567890123456789, 3),
-  ('PQ78RST', 'Audi', 'A4', 2016, 45678901234567890, 4),
-  ('UV90WXY', 'Volkswagen', 'Golf', 2019, 56789012345678901, 5);
+INSERT INTO Car (RegistrationNumber, Make, Model, Year, VIN) VALUES
+  ('AB12CDE', 'Ford', 'Focus', 2015, 12345678901234567),
+  ('FG34HIJ', 'Vauxhall', 'Corsa', 2018, 23456789012345678),
+  ('KL56MNO', 'BMW', '320i', 2017, 34567890123456789),
+  ('PQ78RST', 'Audi', 'A4', 2016, 45678901234567890),
+  ('UV90WXY', 'Volkswagen', 'Golf', 2019, 56789012345678901);
 
 COMMIT;
