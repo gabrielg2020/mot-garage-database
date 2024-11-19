6f19f8bbd47ce3c03658fdbd8f4d603addd5ca36
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-19 13:15:25 +0000

    feat: add SQL script to create Advisory table
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ scripts/tables/10_create_advisory.sql
@@ -0,0 +1,9 @@
+-- Drop table if it exists
+DROP TABLE IF EXISTS Advisory;
+
+-- Create table
+CREATE TABLE Advisory (
+  AdvisoryID INTEGER PRIMARY KEY AUTOINCREMENT,
+  Description TEXT NOT NULL,
+  Severity INTEGER NOT NULL Check (Severity IN ('Low', 'Medium', 'High')),
+);
