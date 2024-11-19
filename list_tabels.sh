59e6cc9e106eade33be4ccbf6d2bdef44fee6823
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-19 11:03:51 +0000

    feat: add script to list and find tables in SQLite database

---
+++ list_tabels.sh
@@ -0,0 +1,29 @@
+# Default action is to list all tables
+action="list_tables"
+table_name=""
+
+while getopts ":f:" opt; do
+  case ${opt} in
+    f )
+      action="find_table"
+      table_name=$OPTARG
+      ;;
+    \? )
+      echo "Usage: $0 [-f <table_name>]"
+      exit 1
+      ;;
+  esac
+done
+
+# Run SQLite command to list tables
+if [ "$action" = "list_tables" ]; then
+  # List all tables
+  sqlite3 "database/mot_garage_db.sqlite" <<EOF
+.tables
+EOF
+elif [ "$action" = "find_table" ]; then
+  # Show schema for the specified table
+  sqlite3 "database/mot_garage_db.sqlite" <<EOF
+.tables '%$table_name%'
+EOF
+fi
