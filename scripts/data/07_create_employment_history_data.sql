83e181533c933b5b2be3efc0822c9b98c6ffa96e
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-19 15:39:05 +0000

    feat: add SQL script to create employment history data
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ scripts/data/07_create_employment_history_data.sql
@@ -0,0 +1,11 @@
+BEGIN TRANSACTION;
+
+-- Insert data
+INSERT INTO EmploymentHistory (GarageID, MechanicID, StartDate, EndDate) VALUES 
+(1, 1, '2021-01-01', NULL),
+(2, 2, '2020-05-15', '2021-05-14'),
+(3, 3, '2019-03-01', NULL),
+(1, 4, '2018-07-01', NULL),
+(2, 5, '2021-06-01', '2022-05-31');
+
+COMMIT;
